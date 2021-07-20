1522eb0b7f0b23df49a122d3e49879f86dffb40f
Avishrant Sharma <avishrants@gmail.com>
2021-07-20 16:10:14 +0530

    Update instructions, add key REDIRECT_AFTER_LOGIN
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>

--- src/AuthMiddleware/middleware.py
+++ src/AuthMiddleware/middleware.py
@@ -7,6 +7,7 @@ AUTH_SETTINGS = getattr(settings, "AUTH_SETTINGS", {})
 LOGIN_URL = AUTH_SETTINGS.get("LOGIN_URL", "login")
 DEFAULT_REDIRECT = AUTH_SETTINGS.get("DEFAULT_REDIRECT", None)
 LOCK_URLS = AUTH_SETTINGS.get("LOCK_URLS", set())
+REDIRECT_AFTER_LOGIN = AUTH_SETTINGS.get("REDIRECT_AFTER_LOGIN", True)
 
 
 class AuthRequiredMiddleware:
@@ -19,12 +20,12 @@ class AuthRequiredMiddleware:
         if len(LOCK_URLS) == 0:
             return self.get_response(request)
 
+        redirect_param = "?next=" + request.path if REDIRECT_AFTER_LOGIN else ""
+
         try:
             current_url = resolve(request.path_info).url_name
             if current_url in LOCK_URLS and not request.user.is_authenticated:
-                return HttpResponseRedirect(
-                    reverse(LOGIN_URL) + "?next=" + request.path
-                )
+                return HttpResponseRedirect(reverse(LOGIN_URL) + redirect_param)
 
             if request.user.is_authenticated and current_url in LOGIN_URL:
                 if DEFAULT_REDIRECT:
